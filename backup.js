9a26832c5f79e9de9347a419781742d041ccdf0a
annabelle <annabellesantos.indeed@gmail.com>
2020-07-09 02:20:17 -0400

    Created answers

--- backup.js
+++ backup.js
@@ -6,48 +6,37 @@ console.log(4 + 5);
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-let a = 1
-let b = 2
-console.log(a+b);
+// your code here
 console.log("answer 1");
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here
-console.log(a-b);
 console.log("answer 2");
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-console.log("I'm writing" + " my first Javascript code");
+// your code here
 console.log("answer 3");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
-console.log(a>b);
+// your code here
 console.log("answer 4");
 
 // 5.) Log an expression that uses modulo, with a result of 4
-console.log(14 % 5);
+// your code here
 console.log("answer 5");
 
 // 6.) Create two different strings and compare them, logging the result.
 // It should be false!
-console.log("this" == "that");
 console.log("answer 6");
 
 // 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
-let str1 = "I'm learning ";
-let str2 = "a bit of ";
-let str3 = "JavaScript at ";
-let str4 = "Kenzie";
-let str_complete = str1.concat(str2,str3,str4);
-console.log(str_complete);
 console.log("answer 7");
 
 // 8.) Uncomment this code and create an expression inside of it.
 // The expression should represent: 5 is less than 10 and 8 is greater than 5
 // This should print true
-console.log(5 < 10 && 8 > 5);
-console.log("answer 8")
+
+// console.log("answer 8")
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-/* console.log(3 * 3 + 1 / 20 / 2 == 1); */
-console.log(((3 * 3) + 1) / (20 / 2) == 1); // This should log true */
+console.log(3 * 3 + 1 / 20 / 2 == 1); // This should log true
